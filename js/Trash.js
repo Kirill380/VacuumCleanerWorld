b6d74894e8b952fc08a2ce60bda1b7421e67b5b2
Kirill Liubun <kreol_123@ukr.net>
2016-03-16 23:36:15 +0200

    Initial commit

---
+++ js/Trash.js
@@ -0,0 +1,57 @@
+(function($) {
+
+	function Trash(options) {
+		var images = ["./img/donat.png", "./img/coca-cola.png", "./img/cookie.png"];
+		var scene = options.scene;
+		var zoom = 0.5;
+		var interval = options.interval || 0;
+		var id;
+
+		/**
+		  * Generate trash in random cell with specified periodicity [interval]
+		  */
+		function startGenerate() {
+			id = setInterval(function() {	
+				var coord = scene.getRandomFreeCell();
+				var trash = scene.renderCell(getRandomImage(), coord.x, coord.y, zoom);
+				trash.transform("r" + _.random(0, 360)); 
+				trash.attr({class:"trash-item"});
+			}, interval);
+		
+		}
+
+		function stopGeneration() {
+			clearInterval(id);
+		}
+
+
+		/**
+		* Randomly generate trash on scene 
+		*/	
+		function staticGeneration() {
+			var times = _.random(0, 20);
+			for (var i = 0; i < times; i++) {
+				var coord = scene.getRandomFreeCell();
+				var amount = _.random(0, 7);
+				for (var j = 0; j < amount; j++) {
+					var trash = scene.renderCell(getRandomImage(), coord.x, coord.y, zoom);
+					trash.transform("r" + _.random(0, 360)); 
+					trash.attr({class:"trash-item"});
+				}
+			}
+		
+		}
+
+
+		function getRandomImage() {
+			return images[_.random(0, images.length - 1)];
+		}
+
+		this.stopGeneration = stopGeneration;
+		this.startGenerate = startGenerate;
+		this.staticGeneration = staticGeneration;
+	} 
+
+	window.Trash = Trash;
+
+})(jQuery);
